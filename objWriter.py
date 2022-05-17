3497950a796bf8bb22340b30113c28df2f889df0
saimtiaz <saymimtiaz@gmail.com>
2022-05-17 16:31:45 -0500

    Initialized objWriter and testing scripts

---
+++ objWriter.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+def objWriter(objects):
+    #Sort by cuboid and spheres
+    cuboids = []
+    spheres = []
+
+    #Remove existing settings.yaml
+    #Create header of the settings.yaml
+        #objective_mode should be set to noECA if there are no objects
+    #Write the cuboids header if there are cuboids
+    #For loop and write each cuboid
+    #For loop and write each sphere
---
+++ test.py
@@ -0,0 +1,19 @@
+from objWriter import objWriter
+
+def test_obj_write():
+    #Create sample list of objects
+    objects = [{'type' : 'sphere', 'scale' : [2.5], 
+                'rotation' : [0.0, 0.0, 0.0], 'translation' : [1.2, 1.2, 1.3]},
+               {'type' : 'cuboid', 'scale' : [1.5, 2.5, 1.6], 
+                'rotation' : [0.0, 0.0, 0.0], 'translation' : [0.2, -1.2, 1.3]},
+    ]
+
+    #Call function
+    objWriter(objects)
+
+
+
+if __name__ == "__main__":
+    #Create sample objects list
+    test_obj_write()
+    print("Everything passed")
