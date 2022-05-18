9593a3149805377b835e2caa7c90e49d077d7435
saimtiaz <saymimtiaz@gmail.com>
2022-05-17 20:13:47 -0500

    Transform pointxyz cluster to object definition

---
+++ cluster2obj.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def cluster2obj(cluster):
+    xS = cluster[0]
+    yS = cluster[1]
+    zS = cluster[2]
+
+    minX = float(min(xS))
+    maxX = float(max(xS))
+
+    minY = float(min(yS))
+    maxY = float(max(yS))
+
+    minZ = float(min(zS))
+    maxZ = float(max(zS))
+
+    cornerPoints = [[minX, minX, minX, minX, maxX, maxX, maxX, maxX],
+                    [minY, maxY, minY, maxY, minY, maxY, minY, maxY],
+                    [minZ, minZ, maxZ, maxZ, minZ, minZ, maxZ, maxZ]
+    ]
+
+    xScale = round(maxX - minX, 2)
+    yScale = round(maxY - minY, 2)
+    zScale = round(maxZ - minZ, 2)
+
+    xTrans = round((maxX + minX)/2, 2)
+    yTrans = round((maxY + minY)/2, 2)
+    zTrans = round((maxZ + minZ)/2, 2)
+
+    cube = {'type' : 'cuboid', 'scale' : [xScale, yScale, zScale], 
+            'rotation' : [0.0, 0.0, 0.0], 'translation' : [xTrans, yTrans, zTrans]}
+    
+
+    fig = plt.figure(figsize=(4,4))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(xS,yS,zS)
+    ax.scatter(cornerPoints[0], cornerPoints[1], cornerPoints[2])
+
+
+    # u = np.linspace(0, 2 * np.pi, 100)
+    # v = np.linspace(0, np.pi, 100)
+    # r = np.sqrt((minX - maxX)**2 + (minY - maxY)**2 + (minZ - maxZ)**2)
+    # print(r)
+    # x = r * np.outer(np.cos(u), np.sin(v)) + (minX + maxX)/2
+    # y = r  * np.outer(np.sin(u), np.sin(v)) + (minY + maxY)/2
+    # z = r  * np.outer(np.ones(np.size(u)), np.cos(v)) + (minZ + maxZ)/2
+    # ax.plot_surface(x, y, z,  rstride=4, cstride=4, color='b', linewidth=0, alpha=0.5)
+    # #calculate vectors for "vertical" circle
+
+
+    
+
+    plt.show()
+    return cube
+
+    #obj = [minX, minX, minX, minX, maxX, maxX, maxX, maxX]
+
+
+
+
--- test.py
+++ test.py
@@ -1,4 +1,19 @@
 from objWriter import objWriter
+from cluster2obj import cluster2obj
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+class Point3D:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+    def display(self):
+        print('x:', self.x, 'y:', self.y, 'z:', self.z)
+    def distance(self, otherPoint):
+        dist = (self.x - otherPoint.x)^2 + (self.y - otherPoint.y)^2 + (self.z - otherPoint.z)^2
+
 
 def test_obj_write():
     #Create sample list of objects
@@ -17,9 +32,39 @@ def test_obj_write():
     #Call function
     objWriter(objects)
 
+def test_cluster2obj():
+    #p1 = Point3D(1,2,3)
+    #p1.display()
+    gaussDist = 0.2
+    amountOfPoints = 200
+    xsFront = (np.random.rand(amountOfPoints, 1) * 7) + (np.random.rand(amountOfPoints, 1) * gaussDist)
+    ysFront = np.random.rand(amountOfPoints, 1) * 2 + (np.random.rand(amountOfPoints, 1) * gaussDist)
+    zsFront = np.random.rand(amountOfPoints, 1) * 3 + (np.random.rand(amountOfPoints, 1) * gaussDist)
 
+    xsTop = np.random.rand(amountOfPoints, 1) * 2 + (np.random.rand(amountOfPoints, 1) * gaussDist)
+    ysTop = (np.random.rand(amountOfPoints, 1) * 0) + 1 + (np.random.rand(amountOfPoints, 1) * gaussDist)
+    zsTop = np.random.rand(amountOfPoints, 1) * 2 + (np.random.rand(amountOfPoints, 1) * gaussDist)
+    
+    xsLeft = (np.random.rand(amountOfPoints, 1) * 3) + 7 + (np.random.rand(amountOfPoints, 1) * gaussDist)
+    ysLeft = np.random.rand(amountOfPoints, 1)  + (np.random.rand(amountOfPoints, 1) * gaussDist)
+    zsLeft = np.random.rand(amountOfPoints, 1) * 5 + (np.random.rand(amountOfPoints, 1) * gaussDist)
 
+    #fig = plt.figure(figsize=(4,4))
+
+    #ax = fig.add_subplot(111, projection='3d')
+
+    allX = np.concatenate((xsFront, xsLeft, xsTop)) 
+    allY = np.concatenate((ysFront, ysLeft, ysTop)) 
+    allZ = np.concatenate((zsFront, zsLeft, zsTop)) 
+    #ax.scatter(allX,allY,allZ) # plot the point (2,3,4) on the figure
+
+    #plt.show()
+
+    cluster = cluster2obj([allX, allY, allZ])
+    print(cluster)
 if __name__ == "__main__":
     #Create sample objects list
-    test_obj_write()
+    #test_obj_write()
+    test_cluster2obj()
     print("Everything passed")
+
